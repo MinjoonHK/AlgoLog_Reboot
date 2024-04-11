3aa7ab047b6b17c0ba941f906d81d93be0a210f6
MinjoonHK <minjoon.park.hk@gmail.com>
2024-04-11 10:06:55 +0900

    test

--- pages/api/auth/nodemailer.ts
+++ pages/api/auth/nodemailer.ts
@@ -13,19 +13,31 @@ const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASSWORD,
   },
 });
-
+// console.log(path.join(__dirname, "/template/emailTemplate.html"));
+// let html = fs.readFileSync("./template/emailTemplate.html", "utf-8");
+// Object.keys(props).forEach((key) => {
+//   html = html.replace(new RegExp(`{{${key}}}`, "g"), props[key]); //props를 {{}} 머스태시 문법으로 html 에 연결
+// });
 export default async function NodeMailer(props) {
-  console.log(path.join(__dirname, "/template/emailTemplate.html"));
-  let html = fs.readFileSync("./template/emailTemplate.html", "utf-8");
-  Object.keys(props).forEach((key) => {
-    html = html.replace(new RegExp(`{{${key}}}`, "g"), props[key]); //props를 {{}} 머스태시 문법으로 html 에 연결
-  });
   let sender = props.sender;
   const info = await transporter.sendMail({
     from: process.env.EMAIL_FROM,
     to: sender,
     subject: "AlgorithmLog 인증메일 입니다",
-    html: html,
+    html: `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
+  <body>
+    <div>
+      <h2>${props.code}</h2>
+      <div>인증번호 입니다</div>
+    </div>
+  </body>
+</html>
+`,
   });
   if (info) {
     return true;
